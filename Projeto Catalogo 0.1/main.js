03dd7a2387e150ca980dd50dda4af9823963d022
Juninho Dias <80465649+juninhodia@users.noreply.github.com>
2022-10-22 14:47:58 -0300

    Update main.js

--- Projeto Catalogo 0.1/main.js
+++ Projeto Catalogo 0.1/main.js
@@ -1,3 +1,5 @@
+//--------------- MENU ----------------------------
+
 const btnMobile = document.getElementById('btn-mobile');
 
 function toggleMenu(event) {
@@ -8,3 +10,91 @@ function toggleMenu(event) {
 
 btnMobile.addEventListener('click', toggleMenu);
 btnMobile.addEventListener('touchstart', toggleMenu)
+
+//----------------- HORARIO DE FUNCIIONAMENTO ----------------
+const data = new Date();
+const diaSemana = data.getDay();
+const horaDia = data.getHours();
+let diaSemanaTexto;
+
+//Domingo
+if (diaSemana === 0 && horaDia >= 9 && horaDia <= 24) {
+    const resultado = document.querySelector('#resultado')
+    resultado.innerHTML = ''
+    const p =document.createElement('p');
+    p.classList.add('paragrafo-resultado-on')
+    p.innerHTML = 'Loja Aberta • 10:00 às 22:00';
+    resultado.appendChild(p);
+} 
+
+//Segunda
+else if (diaSemana === 1 && horaDia >= 9 && horaDia <= 24) {
+    const resultado = document.querySelector('#resultado')
+    resultado.innerHTML = ''
+    const p =document.createElement('p');
+    p.classList.add('paragrafo-resultado-off')
+    p.innerHTML = 'Loja Fechada';
+    resultado.appendChild(p);
+}
+
+//Terça
+else if (diaSemana === 2 && horaDia >= 6 && horaDia <=13) {
+    const resultado = document.querySelector('#resultado')
+    resultado.innerHTML = ''
+    const p =document.createElement('p');
+    p.classList.add('paragrafo-resultado-on')
+    p.innerHTML = 'Loja Aberta • 10:00 às 22:00';
+    resultado.appendChild(p);
+}
+
+//Quarta
+else if (diaSemana === 3 && horaDia >= 6 && horaDia <= 24) {
+    const resultado = document.querySelector('#resultado')
+    resultado.innerHTML
+    const p = document.createElement('p');
+    p.classList.add('paragrafo-resultado-on')
+    p.innerHTML ='Loja Aberta • 10:00 às 22:00';
+    resultado.appendChild(p);
+}
+
+//Quinta
+else if (diaSemana === 4 && horaDia >= 9 && horaDia <= 2) {
+    const resultado = document.querySelector('#resultado')
+    resultado.innerHTML
+    const p = document.createElement('p');
+    p.classList.add('paragrafo-resultado-on')
+    p.innerHTML = 'Loja Aberta • 10:00 às 22:00'
+    resultado.appendChild(p);
+}
+
+//Sexta
+else if (diaSemana === 5 && horaDia >= 9 && horaDia <= 24) {
+    const resultado = document.querySelector('#resultado')
+    resultado.innerHTML
+    const p = document.createElement('p');
+    p.classList.add('paragrafo-resultado-on')
+    p.innerHTML = 'Loja Aberta • 10:00 às 22:00'
+    resultado.appendChild(p);
+}
+
+//Sabado
+else if (diaSemana === 6 && horaDia >= 9 && horaDia <= 24) {
+    const resultado = document.querySelector('#resultado')
+    resultado.innerHTML
+    const p = document.createElement('p');
+    p.classList.add('paragrafo-resultado-on')
+    p.innerHTML = 'Loja Aberta • 10:00 às 22:00'
+    resultado.appendChild(p);
+} 
+
+//  Falso
+else {
+    const resultado = document.querySelector('#resultado');
+    resultado.innerHTML
+    const p = document.createElement('p')
+    p.classList.add('paragrafo-resultado-off')
+    p.innerHTML = 'Loja Fechada'
+    resultado.appendChild(p)
+}
+
+
